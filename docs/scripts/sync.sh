4a9104bee076309d85c4eda9739ea6e722e19eeb
Nan Xiao <me@nanx.me>
2024-10-20 05:14:28 -0400

    Add benchmark article

--- docs/scripts/sync.sh
+++ docs/scripts/sync.sh
@@ -9,3 +9,9 @@ quarto convert docs/articles/get-started.qmd
 jupyter nbconvert --to python docs/articles/get-started.ipynb --output ../../examples/get-started.py
 rm docs/articles/get-started.ipynb
 black examples/get-started.py
+
+quarto render docs/articles/benchmark.qmd
+quarto convert docs/articles/benchmark.qmd
+jupyter nbconvert --to python docs/articles/benchmark.ipynb --output ../../examples/benchmark.py
+rm docs/articles/benchmark.ipynb
+black examples/benchmark.py
