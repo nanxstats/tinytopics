399dc066a5959253a7cfd562d942201ee998490d
Nan Xiao <me@nanx.me>
2024-12-27 01:36:25 -0500

    Add logic to save simulated data

--- examples/distributed.py
+++ examples/distributed.py
@@ -3,6 +3,10 @@
 # accelerate config
 # accelerate launch distributed.py
 
+import os
+
+import torch
+import numpy as np
 from accelerate.utils import set_seed
 
 import tinytopics as tt
@@ -11,9 +15,21 @@ from tinytopics.fit_distributed import fit_model_distributed
 
 def main():
     set_seed(42)
-    n, m, k = 5000, 1000, 10
-    X, true_L, true_F = tt.generate_synthetic_data(n, m, k, avg_doc_length=256 * 256)
-    model, losses = fit_model_distributed(X, k=10)
+    n, m, k = 100_000, 100_000, 20
+    data_path = "X.npy"
+
+    if os.path.exists(data_path):
+        print(f"Loading data from {data_path}")
+        X = torch.from_numpy(np.load(data_path))
+    else:
+        print("Generating synthetic data...")
+        X, true_L, true_F = tt.generate_synthetic_data(
+            n, m, k, avg_doc_length=256 * 256
+        )
+        print(f"Saving data to {data_path}")
+        np.save(data_path, X.cpu().numpy())
+
+    model, losses = fit_model_distributed(X, k=k)
 
     tt.plot_loss(losses, output_file="loss.png")
 
