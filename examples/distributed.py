00a2a2b4808eaffdfc4beaef08f836404c34d232
Nan Xiao <me@nanx.me>
2024-12-27 04:20:41 -0500

    Use separate scripts due to socket.cpp timeout error

--- examples/distributed.py
+++ examples/distributed.py
@@ -5,9 +5,8 @@
 
 import os
 
-import numpy as np
-from accelerate.utils import set_seed
 from accelerate import Accelerator
+from accelerate.utils import set_seed
 
 import tinytopics as tt
 from tinytopics.fit_distributed import fit_model_distributed
@@ -16,25 +15,13 @@ from tinytopics.fit_distributed import fit_model_distributed
 def main():
     accelerator = Accelerator()
     set_seed(42)
-    n, m, k = 100_000, 100_000, 20
+    k = 20
     data_path = "X.npy"
 
-    # Only generate data on main process
-    if accelerator.is_main_process:
-        if os.path.exists(data_path):
-            print(f"Data already exists at {data_path}")
-        else:
-            print("Generating synthetic data...")
-            X, true_L, true_F = tt.generate_synthetic_data(
-                n, m, k, avg_doc_length=256 * 256
-            )
-
-            print(f"Saving data to {data_path}")
-            X_numpy = X.cpu().numpy()
-            np.save(data_path, X_numpy)
-
-    # Wait for main process to finish generating data
-    accelerator.wait_for_everyone()
+    if not os.path.exists(data_path):
+        raise FileNotFoundError(
+            f"Data file {data_path} not found. Run distributed_data.py first."
+        )
 
     print(f"Loading data from {data_path}")
     X = tt.NumpyDiskDataset(data_path)
