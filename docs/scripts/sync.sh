56ff17a2ee565743522579ecc47a5970f73c6a50
Nan Xiao <me@nanx.me>
2025-02-02 22:29:40 -0500

    Use absolute URL logo in README.md

--- docs/scripts/sync.sh
+++ docs/scripts/sync.sh
@@ -34,7 +34,7 @@ for article in get-started benchmark text memory distributed; do
 done
 
 # Sync README.md with modified image path for docs/index.md
-awk '{gsub("docs/assets/logo.png", "assets/logo.png"); print}' README.md > docs/index.md
+awk '{gsub("https://github.com/nanxstats/tinytopics/raw/main/docs/assets/logo.png", "assets/logo.png"); print}' README.md > docs/index.md
 
 # Sync CHANGELOG.md with docs/changelog.md
 cp CHANGELOG.md docs/changelog.md
