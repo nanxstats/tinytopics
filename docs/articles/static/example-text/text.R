919b804283f1759c0c7ff31922479e37241b4045
Nan Xiao <me@nanx.me>
2025-10-19 19:55:06 -0400

    Refactor text data example script to use safetensors as output format

--- docs/articles/static/example-text/text.R
+++ docs/articles/static/example-text/text.R
@@ -5,12 +5,16 @@ curl::curl_download(
 
 load("nips.rda")
 counts_dense <- as.matrix(counts)
-saveRDS(counts_dense, file = "counts.rds")
+counts_tensor <- torch::torch_tensor(counts_dense, dtype = torch::torch_float())
+safetensors::safe_save_file(list(counts = counts_tensor), "counts.safetensors")
 
 writeLines(colnames(counts), con = "terms.txt")
 
 set.seed(42)
 fit <- fastTopics::fit_topic_model(counts, k = 10)
 
-saveRDS(fit$L, file = "L_fastTopics.rds")
-saveRDS(t(fit$F), file = "F_fastTopics.rds")
+L_tensor <- torch::torch_tensor(as.matrix(fit$L), dtype = torch::torch_float())
+safetensors::safe_save_file(list(L = L_tensor), "L_fastTopics.safetensors")
+
+F_tensor <- torch::torch_tensor(t(as.matrix(fit$F)), dtype = torch::torch_float())
+safetensors::safe_save_file(list(F = F_tensor), "F_fastTopics.safetensors")
